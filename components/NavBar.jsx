17642805fa00bf0aef1e561f47f18ab31243d10e
Jimena Hernández <her21199@uvg.edu.gt>
2023-12-11 09:47:26 -0600

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -49,7 +49,7 @@ const NavBar = () => {
               <a href="#" className="-m-1.5 p-1.5">
                 <img
                   className="h-8 w-auto"
-                  src="/images/logo2.png"
+                  src="/assets/images/logo2.png"
                   alt=""
                 />
               </a>
